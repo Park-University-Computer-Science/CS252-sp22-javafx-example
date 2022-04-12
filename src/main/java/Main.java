25bee8daee470c403ecda27e64a7dc3ae4ca2c33
jbkmpark <101652463+jbkmpark@users.noreply.github.com>
2022-04-12 12:52:03 -0500

    Tuesday demo work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.*;
 
 public class Main extends Application {
 
@@ -13,7 +21,56 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         Label l = new Label("CS252 JavaFX Starter Template");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+
+        Button saveNote = new Button("Save");
+        Button loadNote = new Button("Load");
+        saveNote.setDisable(true);
+
+        TextArea noteTakingArea = new TextArea("Enter notes here...");
+
+
+        BorderPane windowLayout = new BorderPane();
+        HBox bottomButtonPanel = new HBox();
+
+        /*
+        StackPane p = new StackPane();
+        p.getChildren().add(saveNote);
+        p.setAlignment(saveNote, Pos.CENTER);
+        */
+        windowLayout.setBottom(bottomButtonPanel);
+        windowLayout.setCenter(noteTakingArea);
+
+        bottomButtonPanel.getChildren().add(loadNote);
+        bottomButtonPanel.getChildren().add(saveNote);
+
+        noteTakingArea.setOnKeyTyped(e -> saveNote.setDisable(false));
+        saveNote.setOnAction(e -> {
+            File saveFile = new File("savedNote.txt");
+            try {
+                PrintWriter fout = new PrintWriter(new FileWriter(saveFile));
+                fout.print(noteTakingArea.getText());
+                fout.close();
+            } catch(IOException err) {
+                System.out.println("ERROR: could not save file");
+            }
+            System.out.println(noteTakingArea.getText());
+            saveNote.setDisable(true);
+        });
+        loadNote.setOnAction(e -> {
+            try {
+                File saveFile = new File("savedNote.txt");
+                BufferedReader fin = new BufferedReader(new FileReader(saveFile));
+                String data = "";
+                while(fin.ready())
+                    data += fin.readLine() +"\n";
+                noteTakingArea.setText(data);
+            } catch(IOException err) {
+                System.out.println("ERROR LOADING FILE");
+            }
+        });
+
+
+        Scene scene = new Scene(windowLayout);
         stage.setScene(scene);
         stage.show();
     }
@@ -23,3 +80,4 @@ public class Main extends Application {
     }
 
 }
+
