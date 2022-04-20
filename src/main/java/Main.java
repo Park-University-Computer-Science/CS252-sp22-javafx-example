6674c24222bcd73a1b47f5cf2cb88f29004051be
jbkmpark <101652463+jbkmpark@users.noreply.github.com>
2022-04-19 23:09:27 -0500

    added saveAs and moved common code to methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,14 +12,72 @@ import java.io.*;
 /** a simple note taking application */
 public class Main extends Application {
 
-    // TODO: add menu items for loading/saving
-
     // area for writing / displaying notes
     private TextArea noteTakingArea = new TextArea("Enter notes here...");
     private Button saveNote = new Button("Save");   // save notes
     private Button loadNote = new Button("Load");   // load saved notes
+    private Stage mainStage = null;
+    private File saveFile = null;
+    private Menu fileMenu = new Menu("File");
+    private Menu helpMenu = new Menu("Help");
+    private MenuBar menu = new MenuBar(fileMenu, helpMenu);
+    private MenuItem loadMenuOption = new MenuItem("Load");
+    private MenuItem saveMenuOption = new MenuItem("Save");
+    private MenuItem saveAsMenuOption = new MenuItem("Save As");
+    private MenuItem aboutMenuOption = new MenuItem("About");
 
 
+    public void chooseFileSave() {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Select a save file for the note");
+        saveFile = fc.showSaveDialog(mainStage);
+    }
+
+    public void chooseFileLoad() {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Select a file to load for the note");
+        saveFile = fc.showOpenDialog(mainStage);
+    }
+
+    public void saveToFile() {
+        if(saveFile == null) { // select a file to save to first:
+            chooseFileSave();
+        }
+        if(saveFile == null) { // if still no file selected, user hit cancel
+            return;
+        }
+        try {
+                PrintWriter fout = new PrintWriter(new FileWriter(saveFile));
+                fout.print(noteTakingArea.getText());
+                fout.close();
+                saveNote.setDisable(true);
+        } catch(IOException err) {
+            Alert dialog = new Alert(Alert.AlertType.ERROR);
+            dialog.setTitle("Error");
+            dialog.setContentText("Could not save note to file: " + saveFile);
+            dialog.showAndWait();
+        }
+    }
+
+    public void loadFile() {
+        chooseFileLoad();
+        if(saveFile == null) { // if still no file selected, user hit cancel
+            return;
+        }
+        try {
+            BufferedReader fin = new BufferedReader(new FileReader(saveFile));
+            String data = "";
+            while(fin.ready())
+                data += fin.readLine() +"\n";
+            noteTakingArea.setText(data);
+            fin.close();
+        } catch(IOException err) {
+            Alert dialog = new Alert(Alert.AlertType.ERROR);
+            dialog.setTitle("Error");
+            dialog.setContentText("Could not load note file: " + saveFile);
+            dialog.showAndWait();
+        }
+    }
 
     /** Set up a maven run profile in intellij or use maven from the command-line.
         Use the javafx:run argument to start the javafx application.
@@ -27,74 +85,46 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) {
-        saveNote.setDisable(true);                  // disable save button when no changes to be saved
+        this.mainStage = stage;
 
-        // enable save button when changes made to textarea
-        noteTakingArea.textProperty().addListener(e -> saveNote.setDisable(false));
+        saveNote.setDisable(true);
+        saveMenuOption.setDisable(true);
 
-        // save notes action
-        saveNote.setOnAction(e -> {
-            File saveFile = null;
-            FileChooser fc = new FileChooser();
-            fc.setTitle("Select a save file for the note");
-            saveFile = fc.showSaveDialog(stage);
-            try {
-                if(saveFile == null) throw new IOException();
-                PrintWriter fout = new PrintWriter(new FileWriter(saveFile));
-                fout.print(noteTakingArea.getText());
-                fout.close();
-                saveNote.setDisable(true);
-            } catch(IOException err) {
-                Alert dialog = new Alert(Alert.AlertType.ERROR);
-                dialog.setTitle("Error");
-                dialog.setContentText("Could not save note to file: " + saveFile);
-                dialog.showAndWait();
-            }
+        // set action listeners
+        noteTakingArea.textProperty().addListener(e -> {
+            saveNote.setDisable(false);
+            saveMenuOption.setDisable(false);
         });
-
-        // load notes action
-        loadNote.setOnAction(e -> {
-            try {
-                File saveFile = new File("savedNote.txt");
-                BufferedReader fin = new BufferedReader(new FileReader(saveFile));
-                String data = "";
-                while(fin.ready())
-                    data += fin.readLine() +"\n";
-                noteTakingArea.setText(data);
-            } catch(IOException err) {
-                System.out.println("ERROR LOADING FILE");
-            }
+        saveNote.setOnAction(e -> saveToFile());
+        saveMenuOption.setOnAction(e -> saveToFile());
+        saveAsMenuOption.setOnAction(e -> {
+            chooseFileSave();
+            saveToFile();
         });
-
-
-        BorderPane windowLayout = new BorderPane();  // root window layout
-        HBox bottomButtonPanel = new HBox();         // pane to hold buttons
-        bottomButtonPanel.setAlignment(Pos.CENTER);  // center the buttons
-        windowLayout.setBottom(bottomButtonPanel);   // put buttons at bottom of window
-        windowLayout.setCenter(noteTakingArea);      // place note editing area in center
-
-
-        // create menu
-        Menu fileMenu = new Menu("File");
-        MenuItem loadMenuOption = new MenuItem("Load");
-        MenuItem saveMenuOption = new MenuItem("Save");
-        MenuItem aboutMenuOption = new MenuItem("About");
+        loadNote.setOnAction(e -> loadFile());
+        loadMenuOption.setOnAction(e -> loadFile());
         aboutMenuOption.setOnAction(e -> {
             var aboutScreen = new Alert(Alert.AlertType.INFORMATION);
             aboutScreen.setTitle("About NoteTakingApp");
             aboutScreen.setContentText("Example JavaFX application for CS252");
             aboutScreen.showAndWait();
         });
+
+        // create menu
         fileMenu.getItems().add(loadMenuOption);
         fileMenu.getItems().add(saveMenuOption);
-        Menu helpMenu = new Menu("Help");
+        fileMenu.getItems().add(saveAsMenuOption);
         helpMenu.getItems().add(aboutMenuOption);
-        MenuBar menu = new MenuBar(fileMenu, helpMenu);
-        windowLayout.setTop(menu);
 
-        // add buttons to button pane
+        // layout elements
+        BorderPane windowLayout = new BorderPane();  // root window layout
+        HBox bottomButtonPanel = new HBox();         // pane to hold buttons
         bottomButtonPanel.getChildren().add(loadNote);
         bottomButtonPanel.getChildren().add(saveNote);
+        bottomButtonPanel.setAlignment(Pos.CENTER);  // center the buttons
+        windowLayout.setBottom(bottomButtonPanel);   // put buttons at bottom of window
+        windowLayout.setCenter(noteTakingArea);      // place note editing area in center
+        windowLayout.setTop(menu);
 
         // add root pane to window scene
         Scene scene = new Scene(windowLayout);
